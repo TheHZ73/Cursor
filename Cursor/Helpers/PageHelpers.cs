695ac2f91fd1aa4c92b5d6c96eac6300b80dfc4f
Dmitry <dddd73@yandex.ru>
2017-11-24 20:15:28 +0400

    Initial commit

---
+++ Cursor/Helpers/PageHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace Cursor
+{
+    class PageHelpers
+    {
+        private IWebDriver driver;
+        private By menu = By.XPath("//img[@src=\"images/menu.jpg\"]");
+        private By menuWithSubMenu = By.XPath("//a[contains(text(),'Menu With Sub Menu')]");
+        private By firstMenuWithSubMenu = By.Id("ui-id-2");
+        private By childMenuWithSubMenu = By.Id("ui-id-3");
+
+        public PageHelpers(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenMenu()
+        {
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(menu));
+            driver.FindElement(menu).Click();
+        }
+
+        public bool OpenSubMenuItem()
+        {
+            IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(menuWithSubMenu));
+            driver.FindElement(menuWithSubMenu).Click();
+            driver.SwitchTo().Frame(1);
+            Actions actions = new Actions(driver);
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(firstMenuWithSubMenu));
+            actions.MoveToElement(driver.FindElement(firstMenuWithSubMenu)).Perform();
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(childMenuWithSubMenu));
+            return driver.FindElement(childMenuWithSubMenu).Displayed;
+        }
+    }
+}
+
+ 
+
---
+++ Cursor/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Cursor
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestCursor()
+        {
+            driver.Navigate().GoToUrl(@"http://way2automation.com/way2auto_jquery/menu.php");
+
+            string login = "TheHZ";
+            string password = "Wizard73";
+            var loginForm = new LoginForm(driver);
+            loginForm.Login(login, password);
+
+            var page = new PageHelpers(driver);
+            page.OpenMenu();
+            Assert.True(page.OpenSubMenuItem());
+        }
+    }
+}
+
